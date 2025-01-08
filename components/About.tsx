a0215f60579e5181a2d95a1da799d55b14c33db3
Ugochukwu Victor <hugotchee1@gmail.com>
2025-01-08 11:10:05 +0100

    first commit

---
+++ components/About.tsx
@@ -0,0 +1,87 @@
+"use client";
+import Image from "next/image";
+import React, { useState } from "react";
+import { WobbleCard } from "./ui/wobble-card";
+import MagicButton from "./ui/magicButton";
+import { IoCopyOutline } from "react-icons/io5";
+import Lottie from "react-lottie";
+import animationData from "@/data/confetti.json";
+
+export function About() {
+  const [copied, setCopied] = useState(false);
+
+  const handleCopy = () => {
+    navigator.clipboard.writeText("hugotchee1@gmail.com");
+
+    setCopied(true);
+  };
+  return (
+    <div className="grid grid-cols-1 lg:grid-cols-3 gap-4 max-w-7xl mx-auto w-full p-10" id='about'>
+      <WobbleCard
+        containerClassName="col-span-1 lg:col-span-2 h-full bg-pink-800 min-h-[500px] lg:min-h-[300px]"
+        className=""
+      >
+        <div className="max-w-xs">
+          <h2 className="text-left text-balance text-base md:text-xl lg:text-3xl font-semibold tracking-[-0.015em] text-white">
+            Gippity AI powers the entire universe
+          </h2>
+          <p className="mt-4 text-left  text-base/6 text-neutral-200">
+            With over 100,000 mothly active bot users, Gippity AI is the most
+            popular AI platform for developers.
+          </p>
+        </div>
+        {/* <Image
+          src="/linear.webp"
+          width={500}
+          height={500}
+          alt="linear demo image"
+          className="absolute -right-4 lg:-right-[40%] grayscale filter -bottom-10 object-contain rounded-2xl"
+        /> */}
+      </WobbleCard>
+      <WobbleCard containerClassName="col-span-1 min-h-[300px] items-center justify-center ">
+        <div>
+            <p className='text-center font-bold text-xl '>Do you want to start a project together?</p>
+        </div>
+        {/* <div className={`absolute -bottom-5 right-0`}>
+          <Lottie
+            options={{
+              loop: copied,
+              autoplay: copied,
+              animationData,
+              rendererSettings: {
+                preserveAspectRatio: "xMidYMid slice",
+              },
+            }}
+          />
+        </div> */}
+        <div className='px-8'>
+        <MagicButton
+          title={copied ? "Email copied" : "Copy my email"}
+          icon={<IoCopyOutline />}
+          position="left"
+          otherClasses="!bg-[#161a31] "
+          handleClick={handleCopy}
+        /></div>
+      </WobbleCard>
+      <WobbleCard containerClassName="col-span-1 lg:col-span-3 bg-blue-900 min-h-[500px] lg:min-h-[600px] xl:min-h-[300px]">
+        <div className="max-w-sm">
+          <h2 className="max-w-sm md:max-w-lg  text-left text-balance text-base md:text-xl lg:text-3xl font-semibold tracking-[-0.015em] text-white">
+            Signup for blazing-fast cutting-edge state of the art Gippity AI
+            wrapper today!
+          </h2>
+          <p className="mt-4 max-w-[26rem] text-left  text-base/6 text-neutral-200">
+            With over 100,000 mothly active bot users, Gippity AI is the most
+            popular AI platform for developers.
+          </p>
+        </div>
+        <Image
+          src="/b5.svg"
+          width={500}
+          height={500}
+          alt="linear demo image"
+          className="absolute -right-10 md:-right-[40%] lg:-right-[20%] -bottom-10 object-contain rounded-2xl"
+        />
+      </WobbleCard>
+    </div>
+  );
+}
---
+++ components/RecentProjects.tsx
@@ -0,0 +1,59 @@
+import React from "react";
+import { projects } from "@/data";
+import { GlareCard } from "./ui/GlareCard";
+import { FaLocationArrow } from "react-icons/fa";
+import Link from "next/link";
+import Image from "next/image";
+import { FaArrowRightFromBracket, FaArrowUpRightFromSquare } from "react-icons/fa6";
+
+const RecentProjects = () => {
+  return (
+    <div className="py-20" id='projects'>
+      <h1 className="heading">
+        A small selection of{" "}
+        <span className="text-purple">recent projects</span>
+      </h1>
+      <div className="flex flex-col gap-8 p-4 mt-10 w-[93vw] mx-auto">
+  {projects.map(({ id, title, des, img, iconLists, link }, index) => (
+    <div
+      key={id}
+      className="lg:min-h-[32.5rem] h-[35.5rem] flex flex-col lg:flex-row rounded-2xl shadow-lg overflow-hidden"
+    >
+      {/* Left Section */}
+      <div className="text-white bg-black w-full lg:w-1/2 flex flex-col gap-6 lg:gap-8 p-6">
+        <h1 className="font-bold text-2xl lg:text-4xl pt-4 text-purple flex items-center gap-2  ">
+        {title} {<Link href={link}><FaArrowUpRightFromSquare /></Link>}
+        </h1>
+        <br className="hidden lg:block" />
+        <p className="text-lg lg:text-3xl italic tracking-wide">{des}</p>
+        <br className="hidden lg:block"/>
+        <div className="flex gap-4 sm:gap-8 flex-wrap">
+          {iconLists.map((list, idx) => (
+            <div
+              key={idx}
+              className="border border-white rounded-2xl py-2 px-4 lg:py-1 lg:px-6 text-sm lg:text-base"
+            >
+              {list}
+            </div>
+          ))}
+        </div>
+      </div>
+
+      {/* Right Section */}
+      <div className="w-full h-full lg:w-1/2 relative">
+        <Image
+          src={img}
+          alt="project Image"
+          layout="fill"
+          className="object-cover"
+        />
+      </div>
+    </div>
+  ))}
+</div>
+
+    </div>
+  );
+};
+
+export default RecentProjects;
